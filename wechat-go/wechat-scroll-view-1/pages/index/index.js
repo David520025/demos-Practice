2b210532c979e32776cbde082e5b1d31d56258cb
David <874800280@qq.com>
2018-12-30 11:24:11 +0800

    wechat demos

---
+++ wechat-go/wechat-scroll-view-1/pages/index/index.js
@@ -0,0 +1,19 @@
+Page({
+  
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+})
---
+++ wechat-go/wechat-swiper/pages/index/index.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    imgUrls: [
+      'http:',
+      'ht',
+      'http',
+      'http:',
+      'ht',
+      'http'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 500,
+    duration: 1000,
+    circular:true
+  }
+})
